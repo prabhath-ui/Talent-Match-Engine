84424345d1c4f531ba74feb77190facca1ad60c8
Prabhath <prabhathsimhadri@gmail.com>
2026-07-01 23:17:28 +0530

    Match Engine.cpp calculator function

--- main.cpp
+++ main.cpp
@@ -8,17 +8,7 @@
 using namespace std;
 
 int main() {
-   vector<string> engSkills = {"C++", "Python", "JavaScript"};
-    SoftwareEngineer alice("SE-01", "Alice Dev", 75.0, 3, 9, engSkills, true);
-    
-    vector<string> designerTools = {"Figma", "Sketch"};
-    UXDesigner bob("UX-01", "Bob Design", 65.0, 2, 4, designerTools, true);
-
-    double aliceScore = MatchEngine::calculateMatchScore(&alice, "Enterprise");
-    double bobScore = MatchEngine::calculateMatchScore(&bob, "Mobile");
-
-    cout<<aliceScore<<"\n";
-    cout<<bobScore;
+   
 
     return 0;
 }
